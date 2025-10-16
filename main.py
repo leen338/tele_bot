7bd472d64412dbbed537bf8a436c7acd2d5d4883
leen338 <lanaleenoz2022@gmail.com>
2025-10-16 17:16:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+TOKEN = "8247936232:AAFtIiF8pFcTfSEDdgrDy6qVyZwp_iqxC1s"
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("بوت شغال!")
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if name == "main":
+    main()
