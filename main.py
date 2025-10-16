312e20e4d81f21659c413e3ab06924f17f36237d
leen338 <lanaleenoz2022@gmail.com>
2025-10-16 18:04:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
+# ضع التوكن مباشرة هنا
 TOKEN = "8247936232:AAFtIiF8pFcTfSEDdgrDy6qVyZwp_iqxC1s"
 
 def start(update: Update, context: CallbackContext):
