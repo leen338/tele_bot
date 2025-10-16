77f7845b6aa25d119480495c63988adcfa8b8ea7
leen338 <lanaleenoz2022@gmail.com>
2025-10-17 00:20:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from flask import Flask
 
 # ===== Flask server dummy =====
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -27,13 +27,14 @@ app_bot = ApplicationBuilder().token(TOKEN).build()
 app_bot.add_handler(CommandHandler("start", start))
 
 # ===== Threads =====
-if name == "main":
+if __name__ == "__main__":
     # شغّل Flask server في Thread منفصل
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
 
     # شغّل Telegram bot
     app_bot.run_polling()
+
 
 
 
